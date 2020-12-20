31f5d678f08d85ef0db6ba14a9cf5defd5610941
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2020-12-20 12:05:16 -0500

    añadido blueprints/date, moment js

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React ,{ useState, useEffect } from 'react'
 import { FormGroup, InputGroup, TextArea } from '@blueprintjs/core';
+import { DateInput } from '@blueprintjs/datetime';
+interface formData {
+    name: String,
+    description: String,
+    initDate: Date,
+    finishDate: Date
+}
 const Form = () => {
+
+    const [dataTask, setDataTask] = useState({});
+    
+    const handleOnChange = ( e ) => {
+        console.log("cambiando..");
+    }
     return(
         <div className = "task-form">
             <h2>Agrega una nueva tarea</h2>
@@ -27,12 +40,29 @@ const Form = () => {
             </FormGroup>
 
             <FormGroup
-                helperText="Helper text with details..."
-                label="Nombre de Tarea"
-                labelFor="text-input"
-                labelInfo="(required)"
+                helperText="dia en que debes iniciar la tarea"
+                label="fecha de nicio"
             >
-                <InputGroup id="text-input" placeholder="Placeholder text" />
+                <DateInput
+                    formatDate={date => date.toLocaleString()}
+                    //onChange={this.handleDateChange}
+                    parseDate={str => new Date(str)}
+                    placeholder={"M/D/YYYY"}
+                    //value={this.state.date}
+                />
+            </FormGroup>
+
+            <FormGroup
+                helperText="dia en que debes terminar la tarea"
+                label="fecha de finalización"
+            >
+                <DateInput
+                    //formatDate={date => date.toLocaleString()}
+                    //onChange={this.handleDateChange}
+                    parseDate={str => new Date(str)}
+                    placeholder={"M/D/YYYY"}
+                    //value={this.state.date}
+                />
             </FormGroup>
         </div>
     );
