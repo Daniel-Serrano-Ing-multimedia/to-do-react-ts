c5521e32512eda1ea1d18ba35109df6fce744147
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2020-12-25 13:58:09 -0500

    añadiendo componente card a la tarea

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
+import { Card, Button } from '@blueprintjs/core';
 import { task } from '../../interfaces/task';
 import './Task.scss';
 
  
 const Task: React.SFC<task> = ( { name, description, initDate, finishDate }: task ) => {
   return ( 
-    <div>
-      <h1> tarea: { name } </h1>
-      <h2> descripcion {description} </h2>
-      <h2> date : {initDate ? initDate.toLocaleDateString() : ""} </h2>
-      <h2> date : { finishDate ? finishDate.toLocaleDateString() : ""} </h2>
-    </div>
+    <Card
+      className = "task"
+      interactive={true}
+      elevation = {3}
+    >
+      <h3> { name } </h3>
+      <p>{description} </p>
+      <p> date : {initDate ? initDate.toLocaleDateString() : "?????"} - {finishDate ? finishDate.toLocaleDateString() : "?????"} </p>
+      <div className = "task__buttons">
+        <Button
+          className = "task__buttons__button"
+         text="Terminada"
+          type = "button" 
+        />
+        <Button
+          className = "task__buttons__button"
+         text="Pendiente"
+          type = "button" 
+        />
+        <Button
+          className = "task__buttons__button"
+         text="Eliminar"
+          type = "button" 
+        />
+      </div>
+    </Card>
    );
 }
  
